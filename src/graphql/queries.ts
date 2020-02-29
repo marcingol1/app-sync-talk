c17aa0b6768b6c1877ebca1b89ae5715c1321bb8
Marcin Gol <golmarcin@gmail.com>
2020-02-29 16:37:46 +0100

    aws synced code

---
+++ src/graphql/queries.ts
@@ -0,0 +1,64 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const syncPosts = /* GraphQL */ `
+  query SyncPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+    $lastSync: AWSTimestamp
+  ) {
+    syncPosts(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      lastSync: $lastSync
+    ) {
+      items {
+        id
+        title
+        rating
+        status
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      title
+      rating
+      status
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        rating
+        status
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,42 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePost = /* GraphQL */ `
+  subscription OnCreatePost {
+    onCreatePost {
+      id
+      title
+      rating
+      status
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onUpdatePost = /* GraphQL */ `
+  subscription OnUpdatePost {
+    onUpdatePost {
+      id
+      title
+      rating
+      status
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onDeletePost = /* GraphQL */ `
+  subscription OnDeletePost {
+    onDeletePost {
+      id
+      title
+      rating
+      status
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
